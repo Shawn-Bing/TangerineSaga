7f37d36a8e4dcbf493a3ba040926d42bd4e1870a
Shawn-Bing <icyoung2020@163.com>
2024-04-15 22:05:35 +0800

    Add Visual Studio Package Player can walk now

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,18 +1,43 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private Rigidbody2D rb; //get component
 
-    // Update is called once per frame
-    void Update()
+    [Header("基本数据")]
+    public float speed;
+    private float inputX;
+    private float inputY;
+    private Vector2 movementInput;
+
+    //detect Player's input then combine it as a Vector
+    private void PlayerInput()
     {
-        
+        inputX = Input.GetAxisRaw("Horizontal");
+        inputY = Input.GetAxisRaw("Vertical");
+
+        //Limit side direction speed
+        if(inputX != 0 && inputY!=0)
+        {
+            inputX = inputX * 0.6f;
+            inputY = inputY * 0.6f;
+        }
+        movementInput = new Vector2(inputX, inputY);
+    }
+    private void Movement()
+    {
+        rb.MovePosition(rb.position + movementInput * speed * Time.deltaTime);
+    }
+    private void Awake() {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    private void Update() {
+        PlayerInput();
+    }
+    private void FixedUpdate() {
+        Movement();
     }
 }
