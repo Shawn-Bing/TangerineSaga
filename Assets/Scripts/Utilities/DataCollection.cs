8cf8f28c860ee76478b868cd5eebacf36ec77c8d
Shawn-Bing <icyoung2020@163.com>
2024-04-26 17:18:52 +0800

    Initialized the bagpack system Add UI builder & UI ToolKit

---
+++ Assets/Scripts/Utilities/DataCollection.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+[System.Serializable]//让Unity能识别用户写的结构体
+public class ItemDetails
+{
+    [Header("物品详情")]
+    public int itemID;
+    public string name;
+    public string itemDescription;
+    public Sprite itemIcon;
+    public ItemType itemType;
+    
+    //世界坐标下的图标不会有描边
+    public Sprite itemOnWorldSprite;
+    [Header("使用方式")]
+    public bool canPickedUp;
+    public bool canDropped;
+    public bool canCarried;
+    [Header("物品属性")]
+    public int itemUseRadius;
+    public int itemPrice;
+
+    [Range(0,1)]//限制下方变量范围是0~1
+    public float sellDiscount;
+
+
+}
---
+++ Assets/Scripts/Utilities/Enums.cs
@@ -0,0 +1,6 @@
+public enum ItemType
+{
+Seed,Commodity,Furniture,//种子、商品、家具
+HoeTool,ChopTool,BreakTool,ReapToop,WaterTool,CollectTool,//各种工具
+ReapableScenery//可破坏场景物品，如杂草
+}
